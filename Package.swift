61f613b8b1293ecc1463f4ec99eb7b7f0d543894
Uwe <uajfriedr@aol.com>
2022-02-17 12:30:09 +0100

    ok

--- Package.swift
+++ Package.swift
@@ -8,11 +8,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
+            name: "LibCPPnew",
+            targets: ["LibCPPnew"]),
+        .library(
             name: "LibFileAttrList",
             targets: ["LibFileAttrList"]),
-        //.library(
-        //    name: "LibCPP",
-        //    targets: ["LibCPP"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,13 +21,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        //.target(
-        //    name: "LibCPP",
-        //    dependencies: ["LibFileAttrList"]
-        //),
+        .target(
+            name: "LibCPPnew",
+            dependencies: []
+        ),
         .target(
             name: "LibFileAttrList",
-            dependencies: []
+            dependencies: ["LibCPPnew"]
         ),
         
         .testTarget(
