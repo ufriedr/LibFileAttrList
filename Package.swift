2b023f87b9a25591804ed50a8d72f860c080f719
Uwe <uajfriedr@aol.com>
2022-02-17 12:39:53 +0100

    ok

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     name: "LibFileAttrList",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "LibCPPnew",
-            targets: ["LibCPPnew"]),
+        //.library(
+        //    name: "LibCPPnew",
+        //    targets: ["LibCPPnew"]),
         .library(
             name: "LibFileAttrList",
             targets: ["LibFileAttrList"]),
@@ -21,13 +21,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LibCPPnew",
-            dependencies: []
-        ),
+        //.target(
+        //    name: "LibCPPnew",
+        //    dependencies: ["LibFileAttrList"]
+        //),
         .target(
             name: "LibFileAttrList",
-            dependencies: ["LibCPPnew"]
+            dependencies: []
         ),
         
         .testTarget(
--- Sources/LibCPPnew/LibFileAttrList-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Header.h
-//  
-//
-//  Created by uwe on 17.02.22.
-//
-
-#import "FileListAttrWrapper.h"
